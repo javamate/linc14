170a4600364ad55882af391000746cac3e34cd73
Eric Broyles <eric.broyles@siemens.com>
2014-05-20 13:44:23 -0700

    Initial commit

---
+++ js/jquery.lithintegration-0.1.js
@@ -0,0 +1,22 @@
+(function( $ ){
+	var settings = {
+	};
+	var methods = {
+		init : function( options ) {
+			// If options exist, lets merge them with our default settings
+			if ( options ) { 
+				$.extend( settings, options );
+			}
+		}
+	}
+	$.fn.lithintegration = function( method ) {
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.lithintegration' );
+		}
+	}
+})( jQuery );
