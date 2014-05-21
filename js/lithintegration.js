dbf9423b3edf741460dee5dd6777482928870348
Eric Broyles <eric.broyles@siemens.com>
2014-05-20 17:10:30 -0700

    Creates unique topics for any given page by base64 encoding the URL if such a topic doesn't already exist.

--- js/lithintegration.js
+++ js/lithintegration.js
@@ -7,6 +7,45 @@ $(document).ready(function() {
 		console.log(data);
 		$('#sessionKey').val(data.response.value);
 	});
+	var pageHash = btoa(window.location.href);
+	console.log(pageHash);
+	$.get(v2ApiBase + 'SELECT * FROM messages where body matches "'+pageHash+'"', function(data) {
+		if (data.response.data.size > 0) {
+			$('#messageId').val(data.response.data.items[0].id);
+			console.log("found a message");
+			$.get(v2ApiBase + 'SELECT id, author, body, post_time FROM messages WHERE topic.id = "'+$('#messageId').val()+'" and depth > 0 order by post_time desc', function(data) {
+				var $responseData = $(data.response.data.items);
+				console.log("raw get response data:");
+				console.log(data);
+				$.each($responseData, function(key, value) {
+					console.log(value);
+					$('#lithBoxMessages').append('<div class="reply"><p class="author">'+value.author.login+'</p><p class="body">'+value.body+'</p></div>');
+				});
+			});
+		} else {
+			console.log("didn't find a message for this page; creating one");
+			var postData = { 
+					'message.body' : 'Check out this content at ' + window.location.href + '<br/>'+pageHash,
+					'message.subject' : document.title,
+					'tag.add' : pageHash
+			};
+			console.log(postData);
+			$.post(v1ApiBase + '/boards/id/Appliances/messages/post?restapi.session_key='+$('#sessionKey').val()+'&restapi.response_format=json&restapi.response_style=-types', postData, function(data) {
+				console.log(data);
+				console.log("message id for page is " + data.response.message.id);
+				$('#messageId').val(data.response.message.id);
+				$.get(v2ApiBase + 'SELECT id, author, body, post_time FROM messages WHERE topic.id = "'+$('#messageId').val()+'" and depth > 0 order by post_time desc', function(data) {
+					var $responseData = $(data.response.data.items);
+					console.log("raw get response data:");
+					console.log(data);
+					$.each($responseData, function(key, value) {
+						console.log(value);
+						$('#lithBoxMessages').append('<div class="reply"><p class="author">'+value.author.login+'</p><p class="body">'+value.body+'</p></div>');
+					});
+				});
+			});
+		}
+	});
 	$('#lithBoxPostButton').click('click.lithintegration', function() {
 		console.log('post button clicked');
 		var postData = { 
@@ -18,15 +57,6 @@ $(document).ready(function() {
 			console.log(data);
 			var html = '<div class="reply"><p class="author">'+data.response.message.author.login+'</p><p class="body">'+data.response.message.body+'</p></div>';
 			$('#lithBoxMessages').prepend(html);
-		});
-	});
-	$.get(v2ApiBase + 'SELECT id, author, body, post_time FROM messages WHERE topic.id = "139" and depth > 0 order by post_time desc', function(data) {
-		var $responseData = $(data.response.data.items);
-		console.log("raw get response data:");
-		console.log(data);
-		$.each($responseData, function(key, value) {
-			console.log(value);
-			$('#lithBoxMessages').append('<div class="reply"><p class="author">'+value.author.login+'</p><p class="body">'+value.body+'</p></div>');
 		});
 	});
 });
